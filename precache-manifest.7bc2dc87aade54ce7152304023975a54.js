2908c63556f118f3e45bae8a513e429de0fa5306
Oleksa <o.antsupov@gmail.com>
2020-06-27 17:31:58 +0300

    Updates

---
+++ precache-manifest.7bc2dc87aade54ce7152304023975a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c981af400649aecc5f06370a269966b1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "20ddf84e429e272099d8",
+    "url": "/todo-app/static/css/main.cd34f1b1.chunk.css"
+  },
+  {
+    "revision": "1ed380a66c2651c5f79b",
+    "url": "/todo-app/static/js/2.d2bc8f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.d2bc8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ddf84e429e272099d8",
+    "url": "/todo-app/static/js/main.6aaa8dc8.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
